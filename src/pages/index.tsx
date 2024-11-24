81dde0e8ffb2768117dd2755bbdebd20fbe48413
zhangbing <zhangbing@bloks.com>
2024-11-24 23:42:48 +0800

    嘿嘿

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,95 @@
-import { handleFile, test } from './tool';
+import { useState } from 'react';
+import { handleFile } from './utils';
+import { Button, Card, Divider, PageHeader, Table, Upload } from 'antd';
+// import result from './resut.json';
+import './index.less';
+import * as XLSX from 'xlsx';
 
 export default function IndexPage() {
+  const [columns, setColumns] = useState<any[]>([]);
+  const [dataSource, setDataSource] = useState<any[]>([]);
+  const [sortedObject, setSortedObject] = useState<any>();
+  const [titleColumn, setTitleColumn] = useState<any>();
+
+  const formatColumns = (columnsData: any) => {
+    const keys = Object.keys(columnsData);
+    // 自定义排序函数
+    const sortedKeys = keys.sort((a, b) => {
+      const numA = parseInt(a.replace(/__EMPTY_?/, ''), 10);
+      const numB = parseInt(b.replace(/__EMPTY_?/, ''), 10);
+      return numA - numB;
+    });
+    const sortedObject = sortedKeys.reduce((acc, key) => {
+      acc[key] = columnsData[key];
+      return acc;
+    }, {} as typeof columnsData);
+    setSortedObject(sortedObject);
+    console.log(sortedObject);
+
+    const formattedColumns = Object.entries(sortedObject).map(
+      ([key, value]) => {
+        return {
+          title: value,
+          dataIndex: key,
+          key: key,
+          render: (text: any) => (
+            <span
+              title={text}
+              style={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+              }}
+            >
+              {text}
+            </span>
+          ),
+        };
+      },
+    );
+    setColumns(formattedColumns);
+  };
+
+  const exportExcel = () => {
+    const newData = [titleColumn, sortedObject, ...dataSource];
+    const wb = XLSX.utils.book_new();
+    const newWorksheet = XLSX.utils.json_to_sheet(newData, {
+      skipHeader: true,
+    });
+    XLSX.utils.book_append_sheet(wb, newWorksheet, 'Sheet1');
+    XLSX.writeFile(wb, 'output.xlsx');
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
-
-      <input type='file' accept='.xls,.xlsx' onChange={(k) => {
-        handleFile(k.target.files?.[0])
-      }}></input>
-
-      <button onClick={test}>Test</button>
+      <input
+        type="file"
+        accept=".xls,.xlsx"
+        onChange={async (k) => {
+          handleFile(k.target.files?.[0], (data) => {
+            if (data) {
+              // [标题, 表头, [数据源]]
+              setTitleColumn(data[0]);
+              formatColumns(data[1]);
+              setDataSource(data.slice(2));
+              console.log('月度总结', data);
+            } else {
+              setDataSource([]);
+            }
+          });
+        }}
+      ></input>
+      <Divider />
+      <Button onClick={exportExcel} type="primary" style={{ marginBottom: 16 }}>
+        导出文件
+      </Button>
+      <Table
+        title={() => '月度总结'}
+        pagination={{ pageSize: 50 }}
+        dataSource={dataSource}
+        columns={columns}
+      ></Table>
     </div>
   );
 }
---
+++ src/pages/utils.ts
@@ -0,0 +1,93 @@
+import * as XLSX from 'xlsx';
+import moment from 'moment-timezone';
+moment.tz.setDefault('Asia/Shanghai');
+function handleJson(json: any[]) {
+  const obj = Object.assign({}, json[1], json[2]);
+  // [标题, 表头, [数据源]]
+  let results: any = [json[0], obj];
+  json.forEach((e) => {
+    const name = e.__EMPTY;
+    if (name && name != '姓名') {
+      const person: any = e;
+      if (person) {
+        // 异常次数
+        const exceptionTimes = Number(person.__EMPTY_14);
+        if (!isNaN(exceptionTimes) && exceptionTimes > 0) {
+          const [dateStr] = person.概况统计与打卡明细?.split(' ');
+          const format = 'YYYY/MM/DD HH:mm';
+          const shouldStart = moment(`${dateStr} 8:30`, format);
+          const shouldEnd = moment(`${dateStr} 17:00`, format);
+          // 最早时间
+          const earliest = moment(
+            `${dateStr} ${person.__EMPTY_7}`,
+            format,
+            true,
+          );
+          // 最晚时间
+          const latest = moment(`${dateStr} ${person.__EMPTY_8}`, format, true);
+          // 弹性时间逻辑
+          if (earliest.isValid() && latest.isValid()) {
+            const deltaStart = earliest.diff(shouldStart, 'minutes');
+            const deltaEnd = latest.diff(shouldEnd, 'minutes');
+            console.log(deltaStart, deltaEnd);
+            if (
+              (deltaStart >= -30 || deltaStart <= 30) &&
+              deltaEnd > deltaStart
+            ) {
+              // 异常次数 -1
+              person.__EMPTY_14 = exceptionTimes - 1;
+            }
+          }
+        }
+        let current = results.find((res: any) => res.__EMPTY === name);
+        if (!current) {
+          results.push(person);
+        } else {
+          Object.entries(current).forEach(([key, value]) => {
+            let count = Number(value);
+            let addCount = Number(person[key]);
+            if (!isNaN(addCount)) {
+              if (isNaN(count)) {
+                count = 0;
+              }
+              count += addCount;
+              if (!Number.isInteger(count)) {
+                count = Number.parseFloat(count.toFixed(1));
+              }
+              current[key] = count;
+            }
+          });
+        }
+      }
+    }
+  });
+  console.log(results);
+  return results;
+}
+
+export function handleFile(
+  file: File | undefined,
+  callBack: (data: any[]) => void,
+) {
+  const fr = new FileReader();
+
+  fr.onload = (x) => {
+    if (x.target && x.target.result) {
+      const fileData = new Uint8Array(x.target.result as ArrayBuffer);
+      const workbook = XLSX.read(fileData, { type: 'array' });
+
+      const sheetName = workbook.SheetNames?.[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const dataJson = XLSX.utils.sheet_to_json<any>(worksheet);
+      const newData = handleJson(dataJson);
+      callBack(newData);
+    } else {
+      callBack([]);
+    }
+  };
+  fr.onerror = (e) => {
+    console.log(e);
+    callBack([]);
+  };
+  fr.readAsArrayBuffer(file!);
+}
